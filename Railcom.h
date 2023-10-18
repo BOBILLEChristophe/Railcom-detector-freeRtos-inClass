6347dd20cbfb7b9917b06961765cb463b4e6884f
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2023-10-18 22:04:05 +0200

    Update Railcom.h

--- Railcom.h
+++ Railcom.h
@@ -12,33 +12,24 @@
 #define __RAILCOM_H__
 
 #include <Arduino.h>
-#include <HardwareSerial.h>
 #include <RingBuf.h>    // https://github.com/Locoduino/RingBuffer
-#include <HardwareSerial.h>
 
 class Railcom
 {
   private:
-    const gpio_num_t m_rxPin;
-    const gpio_num_t m_txPin;
-    const gpio_num_t m_intPin;
+    gpio_num_t m_rxPin;
+    gpio_num_t m_txPin;
     uint16_t m_address;
     QueueHandle_t xQueue1;
     QueueHandle_t xQueue2;
-    HardwareSerial *mySerial;
-    static uint8_t m_compt;
 
     static void IRAM_ATTR receiveData(void *);
     static void IRAM_ATTR parseData(void *);
     static void IRAM_ATTR setAddress(void *);
 
-    static void IRAM_ATTR receiveDataStatic();
-
-    void setup();
-
   public:
-    Railcom(const gpio_num_t, const gpio_num_t); // rxPin - txPin
-    Railcom(const gpio_num_t, const gpio_num_t, const gpio_num_t); // rxPin - txPin - interPin
+    Railcom(const gpio_num_t);
+    Railcom(const gpio_num_t, const gpio_num_t);
     uint16_t address() const;
 };
 
