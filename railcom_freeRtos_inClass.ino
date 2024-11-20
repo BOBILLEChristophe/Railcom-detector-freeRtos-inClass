a09320c87bd59c3da714d1670541ec1057e34855
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2024-11-20 13:49:55 +0100

    Update railcom_freeRtos_inClass.ino

--- railcom_freeRtos_inClass.ino
+++ railcom_freeRtos_inClass.ino
@@ -22,9 +22,9 @@
 #define PROJECT "Railcom Detector ESP32"
 #define AUTHOR  "christophe BOBILLE Locoduino : christophe.bobille@gmail.com"
 
-Railcom railcom_0(GPIO_NUM_3, GPIO_NUM_1);   // Instance de la classe Railcom
-Railcom railcom_1(GPIO_NUM_16, GPIO_NUM_17); // Instance de la classe Railcom
-Railcom railcom_2(GPIO_NUM_13, GPIO_NUM_14); // Instance de la classe Railcom
+Railcom railcom_0(GPIO_NUM_3, UNUSED_PIN);   // Instance de la classe Railcom
+Railcom railcom_1(GPIO_NUM_16, UNUSED_PIN); // Instance de la classe Railcom
+Railcom railcom_2(GPIO_NUM_13, UNUSED_PIN); // Instance de la classe Railcom
 
 void setup()
 {
